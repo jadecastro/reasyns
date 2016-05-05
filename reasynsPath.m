62c305998e16ec8ed5e2b096ed309e6e0ad27cc9
jadecastro <jdc1177@gmail.com>
2016-05-05 16:55:42 -0400

    drake-distro > drake

--- reasynsPath.m
+++ reasynsPath.m
@@ -3,10 +3,11 @@
 com.mathworks.mlwidgets.html.HtmlComponentFactory.setDefaultType('HTMLRENDERER');
 
 addpath(genpath(pwd));
-rmpath(genpath([pwd,'/drake-distro/drake/examples/']));
+rmpath(genpath([pwd,'/drake/drake/examples/']));
 rmpath([pwd,'/lib/ellipsoids/solvers/SeDuMi_1_1']);
 rmpath([pwd,'/lib/mpt/solvers/SeDuMi_1_3']);
 
-run([pwd,'/drake-distro/drake/addpath_drake']);
+
+run([pwd,'/drake/drake/addpath_drake']);
 
 examplesPath = [pwd,'/examples'];
